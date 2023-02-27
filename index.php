7b1fc22919e3430d26db11898ee85229da65e49d
GustavoMABrandao <99413303+GustavoMABrandao@users.noreply.github.com>
2023-02-27 10:55:18 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+      <?php
+           echo "Hello World!";
+      ?>
+    </h1>
+
+    <h2>
+       <?php
+           echo "7 + 3 = 10";
+       ?>
+    </h2>
+        
+    <h2>
+       <?php
+           echo "9 - 5 = 4";
+       ?>
+    </h2>
+
+    <h2>
+       <?php
+           echo "4 * 4 = 16"
+       ?>
+    </h2>
+
+    <h2>
+       <?php
+           echo "9 : 3 = 3"
+       ?>
+    </h2>
+</body>
+
+
+
+</html>
+
